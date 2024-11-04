201e145f81a6d19fa6c2c48f525045f82fc8427e
Abhiram Pinnaka <148970953+Abhiram1502@users.noreply.github.com>
2024-11-04 21:47:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function chargeCal(){
+    var intime=new Date(document.getElementById('start-date').value);
+    var outtime=new Date(document.getElementById('end-date').value);
+    if(intime>=outtime){
+        document.getElementById('result1').innerHTML='Enter out time Correctly' ;
+        return;
+    }
+    const hourlyCharge = 5.00;
+    var min = outtime-intime;
+    var hours= Math.ceil(min/(1000*60*60));
+    var totalCharge = hours*hourlyCharge ;
+    if(hours>12){
+        totalCharge=(12*5)+((hours-12)*4);
+    }else if(hours>24){
+        totalCharge=(24*5)+((hours-24)*3);
+    }
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('fee').textContent = totalCharge.toFixed(2);
+}
